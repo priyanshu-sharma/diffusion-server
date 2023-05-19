2148d788199fe80ac35d7e1eda8c89ff1fe5d50f
priyanshu-sharma <prynsh1112@gmail.com>
2023-05-19 11:52:33 -0700

    added schema for validation

--- src/diffusion_api/image_model_domain/api/web/viewsets/random_diffusion_viewset.py
+++ src/diffusion_api/image_model_domain/api/web/viewsets/random_diffusion_viewset.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from django.http import JsonResponse
+from image_model_domain.api.web.schemas import RandomDiffusionSchema
 
 class RandomDiffusionViewset(viewsets.ViewSet):
     """
@@ -9,14 +10,12 @@ class RandomDiffusionViewset(viewsets.ViewSet):
     """
 
     @action(detail=True, methods=['post'])
-    def initialize_diffusion(self, request):
-        # user = self.get_object()
-        # serializer = PasswordSerializer(data=request.DATA)
-        # if serializer.is_valid():
-        #     user.set_password(serializer.data['password'])
-        #     user.save()
-        #     return Response({'status': 'password set'})
-        # else:
-        #     return Response(serializer.errors,
-        #                     status=status.HTTP_400_BAD_REQUEST)
-        return JsonResponse(request.data, status=status.HTTP_200_OK)
+    def initialize_diffusion(self, request, pk=None):
+        try:
+            data = request.data
+            random_diffusion_schema = RandomDiffusionSchema()
+            serialied_data = random_diffusion_schema.load(data)
+            print(serialied_data)
+            return JsonResponse(request.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return JsonResponse({"Error Message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
